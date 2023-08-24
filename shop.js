d9c4377e121e93ec74be3dcc38bc3403d3c902aa
Zvika Berkovich <zvika0556705015@gmail.com>
2023-08-24 12:42:33 +0300

    cors

--- shop.js
+++ shop.js
@@ -1,5 +1,5 @@
 async function fetchData() {
-    const response = await fetch("https://coati-sweatsuit.cyclic.cloud/api/products");
+    const response = await fetch("https://coati-sweatsuit.cyclic.cloud/api/products",{mode : 'cors'});
     const data = await response.json();
     return data;
 }
